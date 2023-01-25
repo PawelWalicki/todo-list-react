f4b8d7d232cc601a2ec25a6e1d7b04cb48a2a0a9
Pawel Walicki <pawel.walicki89@interia.pl>
2023-01-25 10:28:09 +0100

    added id for task

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,6 +4,7 @@ const Tasks = (props) => (
     <ul className="tasks">
         {props.tasks.map(task => (
             <li
+                key={task.id}
                 className={`tasks__item${task.done && props.hideDone ? " tasks_item--hidden" : ""}`}
             >
                 <button className="tasks__button tasks__button--toggleDone">
@@ -11,7 +12,7 @@ const Tasks = (props) => (
                 </button>
                 <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
                     {task.content}
-                    </span>
+                </span>
                 <button class="tasks__button tasks__button--remove">
                     🗑
                 </button>
